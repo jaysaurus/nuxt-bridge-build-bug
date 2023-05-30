ceda94a32e498224753e9efafeb8ffbbb7b611a6
Jay Edwards <the.jaysaurus@gmail.com>
2023-05-30 16:18:39 +0100

    init commit

---
+++ pages/bah/etc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img src="~/assets/ohHai.png"> &lt;= this image should load when deployed
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
